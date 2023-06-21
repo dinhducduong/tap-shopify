92c3d16761c3a9924ead782b6648046092401e0b
Đinh Đức Dương <97863053+dinhducduong@users.noreply.github.com>
2023-06-21 15:59:45 +0700

    up

--- tap_shopify/streams.py
+++ tap_shopify/streams.py
@@ -189,7 +189,11 @@ class ProductsStream(tap_shopifyStream):
         def preprocess_input(data):
             data_convert = []
             for item in data['products']:
-                data_convert.append(item)
+                data_convert.append({
+                    'id': item['id'],
+                    'sku': item['handle'],
+                    'name': item['body_html']
+                })
             return data_convert
         processed_data = response.json()
         res = preprocess_input(processed_data)
